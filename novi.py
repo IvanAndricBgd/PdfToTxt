25de3fbf6fd54db1c66fcfa51dc914581394a599
IvanAndricBgd <153441409+IvanAndricBgd@users.noreply.github.com>
2024-03-10 21:32:25 +0100

    first commit

---
+++ novi.py
@@ -0,0 +1,19 @@
+import PyPDF2
+
+# Load the PDF file
+with open('/Users/ivan/BOT/pks bot docs/wetransfer_bahreinengleski-pdf_2024-01-12_1338/GUIDE FOR ESTABLISHING A COMPANY, JUNE 2023..pdf', 'rb') as f:
+    pdf = PyPDF2.PdfReader(f)
+    
+    # Extract the text from the PDF
+    text = ''
+    for page in pdf.pages:
+        text += page.extract_text()
+    
+    # Print the extracted text
+        print(text)
+
+
+# Open a file in write mode
+        with open("output.txt", "w") as f:
+    # Write the text to the file
+           f.write(text)
